109a57894de383b6e8a3f8e1593b6c9a3a9699fe
Hossein MJafari <ho3ein.mj.1@gmail.com>
2023-06-10 13:29:17 +0330

    added createRoom().
    - now users can create rooms and it will show up in the main page.

--- base/views.py
+++ base/views.py
@@ -1,11 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
-
-# rooms = [
-#     {'id':1, 'name': 'Let\'s learn python'},
-#     {'id':2, 'name': 'Let\'s learn Django'},
-#     {'id':3, 'name': 'Let\'s learn Back-End'},
-# ]
+from .forms import RoomForm
 
 def home(request):
     rooms = Room.objects.all()
@@ -18,3 +13,16 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room':room}
     return render(request, 'base/room.html', context)
+
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        # print(request.POST)
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
