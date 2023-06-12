4a364173146db878c8c4550f6db95c9df0fac24d
Hossein MJafari <ho3ein.mj.1@gmail.com>
2023-06-12 15:17:30 +0330

    added update and delete for rooms

--- base/urls.py
+++ base/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('room/<str:pk>', views.room, name='room'),
     path('create_room/', views.createRoom, name='create-room'),
+    path('update_room/<str:pk>', views.updateRoom, name='update-room'),
+    path('delete_room/<str:pk>', views.deleteRoom, name='delete-room'),
 ]
