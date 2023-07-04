cb7256aa12e5f9d80a2f7c292ae3d6613649f660
Hossein MJafari <ho3ein.mj.1@gmail.com>
2023-07-04 17:59:49 +0330

    Added user registeration.

--- base/views.py
+++ base/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.db.models import Q
 from .models import Room, Topic
@@ -10,11 +11,13 @@ from .forms import RoomForm
 
 
 def loginPage(request):
+    page = 'login'
+
     if request.user.is_authenticated:
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -29,7 +32,7 @@ def loginPage(request):
             return redirect('home')
         else:
             messages.error(request, "Username Or Password Is Not Correct.")
-    context = {}
+    context = {'page':page}
     return render(request, 'base/login.html', context)
 
 
@@ -37,6 +40,22 @@ def logoutUser(request):
     logout(request)
     return redirect('home')
 
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An Error Occurred During Registeration, Please Try Again.')
+
+    context = {'form':form}
+    return render(request, 'base/login.html', context)
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
