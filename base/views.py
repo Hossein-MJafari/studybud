e5c7b8f79dc4c8e6ab0f927560e1fa0df654aefd
Hossein MJafari <ho3ein.mj.1@gmail.com>
2023-07-04 17:09:37 +0330

    Added Logout.

--- base/views.py
+++ base/views.py
@@ -6,18 +6,6 @@ from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
-def home(request):
-    q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(
-        Q(topic__name__icontains=q) |
-        Q(name__icontains=q) |
-        Q(description__icontains=q)
-        )
-    topics = Topic.objects.all()
-    room_count = rooms.count()
-    context = {'rooms':rooms, 'topics': topics, 'room_count':room_count}
-    return render(request, 'base/home.html', context)
-
 
 def loginPage(request):
 
@@ -39,6 +27,22 @@ def loginPage(request):
             messages.error(request, "Username Or Password Is Not Correct.")
     context = {}
     return render(request, 'base/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+def home(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+    context = {'rooms':rooms, 'topics': topics, 'room_count':room_count}
+    return render(request, 'base/home.html', context)
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
